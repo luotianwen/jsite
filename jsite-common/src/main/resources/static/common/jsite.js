1a000e64863bb326f85561798b44bbba0a5c51f3
lruijun <lruijun.cug@gmail.com>
2018-12-25 17:26:50 +0800

    删除head.html,修改页面显示效果

--- jsite-common/src/main/resources/static/common/jsite.js
+++ jsite-common/src/main/resources/static/common/jsite.js
@@ -506,5 +506,33 @@
     window.js = js;
     window.log = js.log;
     window.error = js.error;
-    window.text = js.text
 })(window.jQuery, window);
+
+
+
+function renderSize(value) {
+    if (null == value || value == "") {
+        return "0 Bytes";
+    }
+    var unitArr = new Array("Bytes","KB","MB","GB","TB","PB","EB","ZB","YB");
+    var index = 0
+        , srcsize = parseFloat(value);
+    index = Math.floor(Math.log(srcsize) / Math.log(1024));
+    var size = srcsize / Math.pow(1024, index);
+    //  保留的小数位数
+    size = size.toFixed(2);
+    return size + unitArr[index];
+}
+
+function getQueryString(name, url) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)","i");
+    if (!url || url == "") {
+        url = window.location.search;
+    } else {
+        url = url.substring(url.indexOf("?"));
+    }
+    r = url.substr(1).match(reg)
+    if (r != null)
+        return unescape(r[2]);
+    return null;
+}
