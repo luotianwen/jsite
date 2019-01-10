20129359da6a244b74884bb1f52603f71178da57
lruijun <lruijun.cug@gmail.com>
2019-01-10 08:50:46 +0800

    修改下载用户导入模板错误

--- jsite-framework/src/main/java/com/jsite/modules/sys/web/UserController.java
+++ jsite-framework/src/main/java/com/jsite/modules/sys/web/UserController.java
@@ -23,7 +23,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -244,7 +243,6 @@ public class UserController extends BaseController {
 	/**
 	 * 下载导入用户数据模板
 	 * @param response
-	 * @param redirectAttributes
 	 * @return
 	 */
 	@RequiresPermissions("sys:user:view")
@@ -258,7 +256,7 @@ public class UserController extends BaseController {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return "redirect:" + adminPath + "/sys/user/list?repage";
+		return "redirect:" + adminPath + "/sys/user/list";
     }
 
 	/**
