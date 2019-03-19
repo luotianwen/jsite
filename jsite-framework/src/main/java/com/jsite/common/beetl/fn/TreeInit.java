74a39ec22727ff79a37ede5397c8995a203c8dfe
lihy <a472159403@163.com>
2019-03-20 00:03:05 +0800

    修复超过三级菜单的显示问题，修改为可展示一级->N级菜单

--- jsite-framework/src/main/java/com/jsite/common/beetl/fn/TreeInit.java
+++ jsite-framework/src/main/java/com/jsite/common/beetl/fn/TreeInit.java
@@ -20,77 +20,60 @@ public class TreeInit implements Function {
 	
 	private String initMenuTree(List<Menu> menuList) {
 		StringBuilder htmlStr = new StringBuilder();
+
 		String rightContainer = "<span class='pull-right-container'><i class='fa fa-angle-left pull-right'></i></span>\n";
 		String href = "";
 		for (Menu menu : menuList) {
-			
+			//子菜单缓冲值
+			StringBuilder htmlStr2 = new StringBuilder();
 			if(menu.getParent().getId().equals("0") && menu.getIsShow().equals("1")) {// 一级菜单isEmpty(menu.parentId)
-				href = StringUtils.isBlank(menu.getHref())?"blank":menu.getHref();
+				href = StringUtils.isBlank(menu.getHref()) ? "blank" : menu.getHref();
 				htmlStr.append("<li class='treeview'>\n");
 				htmlStr.append("<a title='" + menu.getName() + "' href='javascript:' data-href='" + href + "' class='addTabPage'>\n");
 				htmlStr.append("<i class='" + menu.getIcon() + "'></i> <span>&nbsp;" + menu.getName() + "</span> \n");
 				htmlStr.append(rightContainer);
 				htmlStr.append("</a>\n");
-				
-				if(href.equals("blank")) {
-					htmlStr.append("<ul class='treeview-menu'>");
-					
-					for(Menu menu1 : menuList) {
-						if(menu1.getParent().getId().equals(menu.getId()) && menu1.getIsShow().equals("1")) {// 二级菜单
-							href = StringUtils.isBlank(menu1.getHref())?"blank":menu1.getHref();
-							
-							htmlStr.append(href.equals("blank")?"<li class='treeview'>\n":"");
-							htmlStr.append(href.equals("blank")?"<a title='" + menu1.getName() + "' href='javascript:' data-href='" + href + "' class='addTabPage'>\n":"");
-							htmlStr.append(href.equals("blank")?"<i class='" + menu1.getIcon() + "'></i>&nbsp;" + menu1.getName() + " \n":"");
-							htmlStr.append(href.equals("blank")?rightContainer:"");
-							htmlStr.append(href.equals("blank")?"</a>\n":"");
-							
-							htmlStr.append(href.equals("blank")?"":"<li class='treeview'><a title='" + menu1.getName() + "' href='javascript:' data-href='" + href + "' class='addTabPage'><i class='" + menu1.getIcon() + "'></i>&nbsp;" + menu1.getName() + "</a></li>\n");
-							
-							if(href.equals("blank")) {
-								htmlStr.append("<ul class='treeview-menu'>");
-								
-								for(Menu menu2 : menuList) {
-									if(menu2.getParent().getId().equals(menu1.getId()) && menu2.getIsShow().equals("1")) {// 三级菜单
-										href = StringUtils.isBlank(menu2.getHref())?"blank":menu2.getHref();
-										
-										htmlStr.append(href.equals("blank")?"<li class='treeview'>\n":"");
-										htmlStr.append(href.equals("blank")?"<a title='" + menu2.getName() + "' href='javascript:' data-href='" + href + "' class='addTabPage'>\n":"");
-										htmlStr.append(href.equals("blank")?"<i class='" + menu2.getIcon() + "'></i>&nbsp;" + menu2.getName() + " \n":"");
-										htmlStr.append(href.equals("blank")?rightContainer:"");
-										htmlStr.append(href.equals("blank")?"</a>\n":"");
-										
-										htmlStr.append(href.equals("blank")?"":"<li class='treeview'><a title='" + menu2.getName() + "' href='javascript:' data-href='" + href + "' class='addTabPage'><i class='" + menu2.getIcon() + "'></i>&nbsp;" + menu2.getName() + "</a></li>\n");
-										
-										if(href.equals("blank")) {
-											htmlStr.append("<ul class='treeview-menu'>");
-											
-											for(Menu menu3 : menuList) {
-												if(menu3.getParent().getId().equals(menu2.getId()) && menu3.getIsShow().equals("1")) {// 四级菜单
-													// TODO  
-													
-													
-												}
-											}
-										}
-										
-									}
-								}
-								
-								htmlStr.append("</ul>\n");
-							}
-						}
-					}
-					
-					htmlStr.append("</ul>\n");
-				}
-				
-				htmlStr.append("</li>\n");
+				//递归获取二级->N级菜单
+				StringBuilder subMenu = subMenu(menuList, menu, href, htmlStr2, rightContainer);
+				htmlStr.append(subMenu);
 			}
-			
 		}
-		
 		return htmlStr.toString();
+	}
+
+	/**
+	 * 递归获取二级->N级菜单
+	 * @param menuList
+	 * @param menu
+	 * @param href
+	 * @param htmlStr2
+	 * @param rightContainer
+	 * @author lihy
+	 * @data 2019年3月19日23:43
+	 * @return
+	 */
+	public StringBuilder subMenu(List<Menu> menuList,Menu menu,String href,StringBuilder htmlStr2,String rightContainer){
+		//获取每级菜单
+		if(href.equals("blank")) {
+			htmlStr2.append("<ul class='treeview-menu'>");
+			for(Menu menu1 : menuList) {
+				if(menu1.getParent().getId().equals(menu.getId()) && menu1.getIsShow().equals("1")) {// 四级菜单
+					href = StringUtils.isBlank(menu1.getHref())?"blank":menu1.getHref();
+
+					htmlStr2.append(href.equals("blank")?"<li class='treeview'>\n":"");
+					htmlStr2.append(href.equals("blank")?"<a title='" + menu1.getName() + "' href='javascript:' data-href='" + href + "' class='addTabPage'>\n":"");
+					htmlStr2.append(href.equals("blank")?"<i class='" + menu1.getIcon() + "'></i>&nbsp;" + menu1.getName() + " \n":"");
+					htmlStr2.append(href.equals("blank")?rightContainer:"");
+					htmlStr2.append(href.equals("blank")?"</a>\n":"");
+
+					htmlStr2.append(href.equals("blank")?"":"<li class='treeview'><a title='" + menu1.getName() + "' href='javascript:' data-href='" + href + "' class='addTabPage'><i class='" + menu1.getIcon() + "'></i>&nbsp;" + menu1.getName() + "</a></li>\n");
+					subMenu(menuList,menu1, href, htmlStr2, rightContainer);
+				}
+			}
+			htmlStr2.append("</ul>\n");
+		}
+
+		return htmlStr2;
 	}
 
 }
